fae28a4a04cbd603bd09f5355228da2d74c3f1a6
Saran-Avinash <saranbaskarmsp@gmail.com>
2025-04-06 12:57:12 +0530

    udpate: mysql2

--- frontend/src/GetAttendence.jsx
+++ frontend/src/GetAttendence.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { TableExport } from "tableexport";
 
 const GetAttendence = () => {
     
@@ -8,45 +9,62 @@ const GetAttendence = () => {
        const [isPending, setIsPending] = useState(false);
        const [isError, setIsError] = useState(false);
        const [filteredColumns, setFilteredColumns] = useState([]);
-       let i = filteredColumns.length;
+       const periodNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+       let date = new Date();
        let j = 0;
+       let totalPeriods = 0;
+       let candidatePeriodAttendance = 0;
  
-       function fetchDetails(){
+       function exportFile(){
+        // new TableExport(document.getElementsByTagName("table"));
+        let table = document.getElementsByTagName("table");
+        TableToExcel.convert(table[0], {
+            name : `${(new Date()).getTime()}-cse-c.xlsx`,
+            sheet : {
+                name : 'Sheet 1'
+            }
+        
+    
+        });
+    }
+       async function  fetchDetails(){
 
         const payLoad = {
             startDate : fromDate,
             endDate : toDate
         }
 
-        fetch("http://localhost:8080/getAttendence", {
+        fetch("http://localhost:8080/getAttendance", {
             'method' : 'POST',
             'headers' : {
                 'Content-Type' : 'application/json',
             },
             'body' : JSON.stringify(payLoad)
         }).then(
-            response => {
+           async  response => {
                 
                 try{
                   if(!response.ok){
                     setIsError(true);
                     setIsPending(false);
                   }
-                  return response.json();
+                  const result = await response.json();
+                //   console.log(result);
+                  return result;
                 }
                 catch(error){
-                    
+                    setIsError(true);
                     console.log(error);
                 }
             }
         ).then(result => {
 
-            console.log(details);
-            console.log(filteredColumns);
+            console.log(result);
+            // console.log(filteredColumns);
             setIsError(false);
-            setDetails(result.result);
+            setDetails(result);
             setIsPending(false);
-            setFilteredColumns(result.filteredColumns);
+            // setFilteredColumns(result.filteredColumns);
         })
        }
     
@@ -86,7 +104,72 @@ const GetAttendence = () => {
              </h3></div>}
           {details && <div className="table-container">
                     
+
                     <table className="table">
+                        <thead>
+                            <tr>
+                                <th rowSpan={2}>S.No</th>
+                                <th rowSpan={2}>Roll No</th>
+                                {[...new Set(Object.values(details).flatMap(dates => Object.keys(dates)))].map(date =>{
+                                    const dateObj = new Date(date);
+                                    const formattedDate = dateObj.toLocaleDateString("en-IN");
+                                    return (
+
+                                    <th key={`${formattedDate}`}colSpan={8}>{formattedDate}</th>
+                                );})
+                            }
+                             <th rowSpan={2}>Total Periods</th> 
+                             <th rowSpan={2}>Attended Periods</th>
+                             <th rowSpan={2}>Period vise <br></br> Attendance Percentage</th>
+                            </tr>
+                            <tr>
+                                {
+                                    [...new Set(Object.values(details).flatMap(dates => Object.keys(dates)))].map(date => {
+                                       return(
+                                        periodNumbers.map(period => {
+                                            console.log(period);
+                                            return(<th key={`${date}-${period}`}>{period}</th>);
+                                        
+                                        })
+                                       );
+                                        // return res
+                                    })
+                                }
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.entries(details).map(([rollno, dates]) => {
+                                totalPeriods = 0;
+                                candidatePeriodAttendance = 0;
+                                return(
+                                    <tr key={`${rollno}-j`}>
+                                    <td>{++j}</td>
+                                    <td>{rollno}</td>
+                                    {Object.entries(dates).map(([date, periods]) => {
+                                      return (
+                                        <>
+                                            {Object.entries(periods).map(([period, status]) => {
+                                                totalPeriods++;
+                                                status == "P" ? candidatePeriodAttendance++ : '';
+                                                return(
+                                                    <>
+                                                        {/* <td key={`${rollno}-${period}`}>{period}</td> */}
+                                                        
+                                                        <td className={status == "A" ? `red-text` : ''} key={`${rollno}-${period}-${status}`}>{status}</td>
+                                                    </>
+                                                )
+                                            }) }
+                                        </>
+                                      )
+                                    })}
+                                    <td>{totalPeriods}</td>
+                                    <td>{candidatePeriodAttendance}</td> 
+                                    <td>{((candidatePeriodAttendance / totalPeriods)*100).toFixed(2)}</td>
+                                </tr>)
+                            })}
+                        </tbody>
+                    </table>
+                     {/* <table className="table">
                         <thead>
                             <tr>
                                 <th>S.No</th>
@@ -128,8 +211,9 @@ const GetAttendence = () => {
                                 })
                             }
                         </tbody>
-                    </table>
+                    </table>  */}
             </div>}
+            <button onClick={exportFile}>Click to Export</button>
         </>
      );
 }
